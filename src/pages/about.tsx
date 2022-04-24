35900b5e1baf1941a606ef320f73606832a1fb35
AT108 <aneatrats@gmail.com>
2022-04-24 12:04:43 +0000

    workshop update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,9 +4,18 @@ import { Content } from '../content/Content';
 import { Meta } from '../layout/Meta';
 import { Main } from '../templates/Main';
 
+const Info = ({ text }: { text: string }) => (
+  <p className="text-red-600">{text}</p>
+);
+
 const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <Content>
+      <Info text={'text'} />
+      <Info text={'text2'} />
+      <Info text={'text3'} />
+
+      <p>Hello</p>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
         recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
---
+++ src/pages/projects.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Project = () => <h1>asd</h1>;
+
+export default Project;
